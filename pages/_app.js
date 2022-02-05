10b13f162c9ba6cbd9b3a371f38d306ea98b5009
Ahmet Erturk <aesatiroglu@gmail.com>
2022-02-05 03:47:35 +0300

    first

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+
+function greftApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
+
+
+export default greftApp
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function homePage() {
+    return <div>Welcome to Next.js!</div>
+}
+
+export default homePage
